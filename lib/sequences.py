051625efe5f233fe5259cf22f27217083c38c5f0
faith wangare <faithwangare750@gmail.com>
2023-05-21 21:32:22 +0300

    complete lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fib_sequence = []
+    if length >= 1:
+        fib_sequence.append(0)
+    if length >= 2:
+        fib_sequence.append(1)
+    for i in range(2, length):
+        fib_sequence.append(fib_sequence[i-1] + fib_sequence[i-2])
+    print(fib_sequence)
+    
+print_fibonacci(9)
